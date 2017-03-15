10d936c21aab6a31e77e2c53f013c51d796be3ca
Colin Bloom <colinjbloom113@gmail.com>
2017-03-14 20:14:15 -0700

    add functionality for searching by year

--- App.java
+++ App.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class App {
   public static void main(String[] args) {
-    Console userConsole = System.readLine();
+    Console userConsole = System.console();
 
     List<CD> allCD = new ArrayList<CD>();
 
@@ -17,13 +17,30 @@ public class App {
       System.out.println("Welcome to CDworld! Please enter one of the following: 'See all inventory', 'Search based on release year', 'Search based on price range', 'Search by artist', 'Search by album name'.");
       String userChoice = userConsole.readLine();
 
-      if ( userChoice == "See all inventory" ) {
+      if ( userChoice.equals("See all inventory") ) {
         for ( CD eachCD : allCD ) {
           System.out.println("-------------");
           System.out.println( eachCD.mArtistName );
           System.out.println( eachCD.mAlbumName );
           System.out.println( eachCD.mYear );
           System.out.println( eachCD.mPrice );
+        }
+      } else if ( userChoice.equals("Search based on release year") ) {
+        System.out.println("What year is the EARLIEST year that you are interested in?");
+        String stringFloorYear = userConsole.readLine();
+        Integer floorYear = Integer.parseInt(stringFloorYear);
+        System.out.println("What year is the LATEST year that you are interested in?");
+        String stringCeilingYear = userConsole.readLine();
+        Integer ceilingYear = Integer.parseInt(stringCeilingYear);
+
+        for ( CD eachCD : allCD ) {
+          if ( eachCD.mYear >= floorYear || eachCD.mYear <= ceilingYear ) {
+            System.out.println("-------------");
+            System.out.println( eachCD.mArtistName );
+            System.out.println( eachCD.mAlbumName );
+            System.out.println( eachCD.mYear );
+            System.out.println( eachCD.mPrice );            
+          }
         }
       }
     }
