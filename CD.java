90e2385670495a7db94d0371a34ab5f07e442513
Colin Bloom <colinjbloom113@gmail.com>
2017-03-14 19:52:17 -0700

    initial commit, create CD class

---
+++ CD.java
@@ -0,0 +1,13 @@
+class CD {
+  public String mArtistName;
+  public String mAlbumName;
+  public Integer mYear;
+  public Integer mPrice;
+
+  public CD(String artistName, String albumName, Integer year, Integer price) {
+    mArtistName = artistName;
+    mAlbumName = albumName;
+    mYear = year;
+    mPrice = price;
+  }  
+}
